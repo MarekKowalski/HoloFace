3b2847d03b906e1cf51d3d87394c14eccd42c970
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-23 11:34:00 +0800

    Delete HoloFace directory

--- HoloFace/Assets/IAnimationTrigger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IAnimationTrigger
-{
-    void AttributePresent();
-    void AttributeNotPresent();
-}
